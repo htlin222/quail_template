50fabe58d04ad331a7a9f269769b6c9f59dc92e3
htlin222 <hsieh.ting.lin@gmail.com>
2023-05-06 12:36:44 +0800

    add image

--- tw_im_board_template/genq.sh
+++ tw_im_board_template/genq.sh
@@ -8,6 +8,9 @@ for i in {1..160}; do
   echo "<p>Question $i:<br />" > $filename
   echo "<p>題幹<br />" >> $filename
   echo "<br />" >> $filename
+  echo "<img src='./001-img-1.jpg'><br />" >> $filename
+  echo "<!--圖片的範例如上-->" >> $filename
+  echo "<br />" >> $filename
   echo "A. Answer <br />" >> $filename
   echo "B. Answer <br />" >> $filename
   echo "C. Answer <br />" >> $filename
@@ -19,8 +22,15 @@ for i in {1..160}; do
   filename=$(printf "%03d-s.html" $i)
   touch $filename
   echo "<p>Correct Answer: A - blablabla <br />" > $filename
+  echo "<!--↑↑↑↑↑正確選項一定要長這樣↑↑↑↑↑-->" >> $filename
   echo "<br />" >> $filename
-  echo "詳解在這裡</p>" >> $filename
+  echo "<!--斷行一定要加一個<br /> 不然會糊成一大段-->" >> $filename
+  echo "<img src='./001-img-1.jpg'><br />" >> $filename
+  echo "<!--圖片的範例如上-->" >> $filename
+  echo "<br />" >> $filename
+  echo "詳解在這裡" >> $filename
+  echo "</p>" >> $filename
+  echo "<!--最後要有一個/p表示這段結束-->" >> $filename
 done
 
 exit 0

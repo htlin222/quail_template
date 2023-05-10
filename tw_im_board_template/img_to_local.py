4905e09b6c2d11e6ba8604f3071855ed12410709
HsiehTing Lin <hsieh.ting.lin@gmail.com>
2023-05-11 00:42:18 +0800

    update code

---
+++ tw_im_board_template/img_to_local.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# title: img_to_local
+# date: "2023-05-11"
+import os
+import requests
+import re
+
+# Define a regular expression pattern to match image links in Markdown
+pattern = r'\!\[(.*)\]\((http.*?)\)'
+
+# Define a function to download an image and save it to disk
+def download_image(url, file_path):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
+        return True
+    else:
+        return False
+
+# Loop over all Markdown files in the current directory
+for md_file in os.listdir():
+    if md_file.endswith('.md'):
+        # Get the base directory for the Markdown file
+        base_dir = os.path.abspath(os.path.dirname(md_file))
+
+        # Read the contents of the Markdown file
+        with open(md_file, 'r') as f:
+            md_content = f.read()
+
+        # Find all remote image links in the Markdown content
+        for match in re.finditer(pattern, md_content):
+            image_alt_text = match.group(1)
+            image_url = match.group(2)
+
+            # Generate a local file path for the image based on its URL
+            image_file_name = os.path.basename(image_url)
+            image_file_path = os.path.join(base_dir, image_file_name)
+
+            # Download the image if it doesn't already exist in the local directory
+            if not os.path.exists(image_file_path):
+                if download_image(image_url, image_file_path):
+                    print(f'Downloaded image: {image_url} -> {image_file_path}')
+                else:
+                    print(f'Error downloading image: {image_url}')
+
+            # Replace the remote image link with a local image path in the Markdown content
+            local_image_path = os.path.relpath(image_file_path, base_dir)
+            md_content = md_content.replace(match.group(0), f'![{image_alt_text}]({local_image_path})')
+
+        # Save the updated Markdown content to the original file
+        with open(md_file, 'w') as f:
+            f.write(md_content)
--- tw_im_board_template/magic.py
+++ tw_im_board_template/magic.py
@@ -5,8 +5,12 @@
 
 import markdown
 import re
+import csv
+import json
 import os
 
+json_file_path = 'index.json'
+tagnames_file_path = 'tagnames.json'
 
 def splite_the_main(main_file):
 
@@ -45,11 +49,54 @@ def split_by_h3(filename):
         f.write(markdown.markdown(fragments[0], output_format='html'))
 
 
+def open_csv_file(csv_file_path):
+
+    # Open the CSV file and read its contents
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        header = next(csv_reader)  # Get the header row
+
+        # Store the original header in a dictionary
+        tags_dict = {}
+        for i in range(len(header)-1):
+            tags_dict[str(i)] = header[i+1]
+
+        # Create a new header with values "0" and "1"
+        new_header = ["0", "1"]
+
+        # Convert each row to a dictionary
+        data_dict = {}
+        for row in csv_reader:
+            key = row[0].zfill(3)  # Pad the key with leading zeros
+            data_dict[key] = {new_header[i]: row[i+1] for i in range(len(new_header))}
+
+    # Write the resulting dictionary as JSON
+    with open(json_file_path, 'w', encoding='utf-8') as jsonfile:
+        json.dump(data_dict, jsonfile, ensure_ascii=False, indent=4, separators=(',', ': '))
+
+        # Add a newline character after each item in the JSON output
+        jsonfile.write('\n')
+
+    # Write the original header as a separate JSON file
+    with open(tagnames_file_path, 'w', encoding='utf-8') as tagsfile:
+        tags_dict = {"tagnames": tags_dict}
+        json.dump(tags_dict, tagsfile, ensure_ascii=False, indent=4, separators=(',', ': '))
+
+        # Add a newline character after each item in the JSON output
+        tagsfile.write('\n')
+    print("Generated two JSON files")
+
+
 def main():
     directory = '.'  # replace with the path to your directory if necessary
     for filename in os.listdir(directory):
         if filename.endswith('.md'):
             splite_the_main(filename)
+    print("Generated All the HTML files")
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.csv'):
+            open_csv_file(filename)
 
     print('Done')
 

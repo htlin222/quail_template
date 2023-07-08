496156a68492735099a03113b961a64a3ad01dc3
htlin222 <hsieh.ting.lin@gmail.com>
2023-07-08 22:22:18 +0800

    add--cloze

--- template/create_apkg.py
+++ template/create_apkg.py
@@ -2,20 +2,26 @@
 # -*- coding: utf-8 -*-
 # title: create_apkg
 # date: "2023-05-18"
-import sys
+import glob
 import os
 import re
-import subprocess
 import shutil
-import glob
+import subprocess
+import sys
 
 
 def replace_markdown(text):
-    text = re.sub(r'^##\s', r'<div data-question markdown="block">\n\n## ', text, flags=re.MULTILINE)
-    text = re.sub(r'^###\s', r'</div>\n### ', text, flags=re.MULTILINE)
+    text = re.sub(
+        r"^##\s",
+        r'<div data-question markdown="block">\n\n## ',
+        text,
+        flags=re.MULTILINE,
+    )
+    text = re.sub(r"^###\s", r"</div>\n### ", text, flags=re.MULTILINE)
     return text
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
         file_dir, file_name = os.path.split(file_path)
@@ -27,16 +33,16 @@ if __name__ == '__main__':
         # Create input_for_anki directory if it doesn't exist
         os.makedirs(input_dir, exist_ok=True)
 
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             markdown_text = file.read()
             modified_text = replace_markdown(markdown_text)
 
-        with open(input_path, 'w') as file:
+        with open(input_path, "w") as file:
             file.write(modified_text)
             print(f"Modified Markdown file has been saved to {input_path}")
 
         # Copy images to input_for_anki directory
-        image_extensions = ['.jpg', '.png', '.jpeg']
+        image_extensions = [".jpg", ".png", ".jpeg"]
         image_files = [f for ext in image_extensions for f in glob.glob(f"*{ext}")]
         for image_file in image_files:
             shutil.copy(image_file, input_dir)
@@ -46,13 +52,13 @@ if __name__ == '__main__':
 
         # Check if mdankideck is installed, and if not, install it
         try:
-            subprocess.run(['mdankideck', '--version'], check=True)
+            subprocess.run(["mdankideck", "--version"], check=True)
         except FileNotFoundError:
             print("mdankideck not found. Installing markdown-anki-decks package...")
-            subprocess.run(['pip', 'install', 'markdown-anki-decks'])
+            subprocess.run(["pip", "install", "markdown-anki-decks"])
 
         # Run mdankideck subprocess
-        subprocess.run(['mdankideck', input_dir, output_dir])
+        subprocess.run(["mdankideck", input_dir, output_dir, "--cloze"])
         print(f"mdankideck process completed. Output files are saved in {output_dir}")
 
         # Delete input_for_anki folder

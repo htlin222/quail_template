efc2ea70bff15579baff7c76077b6aa1328c517f
htlin222 <hsieh.ting.lin@gmail.com>
2023-08-22 00:37:46 +0800

    Routine: Upload

--- pdf_processing/pdf_to_md_and_img.py
+++ pdf_processing/pdf_to_md_and_img.py
@@ -2,71 +2,71 @@
 # -*- coding: utf-8 -*-
 # title: takeimages
 # date: "2023-05-08"
-
-#Import required dependencies
-import fitz
+# Import required dependencies
 import os
+
+import fitz
 import pdfplumber
 from PIL import Image
 
-directory = '.'
-#Get list of PDF files in current directory
-pdf_files = [f for f in os.listdir('.') if f.endswith('.pdf')]
+directory = "."
+# Get list of PDF files in current directory
+pdf_files = [f for f in os.listdir(".") if f.endswith(".pdf")]
 
 # All PDF to markdown
 for filename in os.listdir(directory):
-    if filename.endswith('.pdf'):
+    if filename.endswith(".pdf"):
         filepath = os.path.join(directory, filename)
         with pdfplumber.open(filepath) as pdf:
-            text = ''
+            text = ""
             for page in pdf.pages:
                 text += page.extract_text()
                 print(text)
         # Save the extracted text as a markdown file
-        md_filename = os.path.splitext(filename)[0] + '_未處理.md'
-        with open(md_filename, 'w') as f:
+        md_filename = os.path.splitext(filename)[0] + "_未處理.md"
+        with open(md_filename, "w") as f:
             f.write(text)
-#Loop over PDF files to extract image
+# Loop over PDF files to extract image
 for file_path in pdf_files:
-    #Open PDF file
+    # Open PDF file
     pdf_file = fitz.open(file_path)
 
-    #Get the number of pages in PDF file
+    # Get the number of pages in PDF file
     page_nums = len(pdf_file)
 
-    #Create directory for saved images if it does not exist
-    images_dir = os.path.join(os.path.splitext(file_path)[0] + '_圖片')
+    # Create directory for saved images if it does not exist
+    images_dir = os.path.join(os.path.splitext(file_path)[0] + "_fig")
     if not os.path.isdir(images_dir):
         os.makedirs(images_dir, exist_ok=True)
 
-    #Create empty list to store images information
+    # Create empty list to store images information
     images_list = []
 
-    #Extract all images information from each page
+    # Extract all images information from each page
     for page_num in range(page_nums):
         page_content = pdf_file[page_num]
         images_list.extend(page_content.get_images())
 
     print(f"{os.path.splitext(file_path)[0]}，本文件共{page_num}頁")
 
-    #Raise error if PDF has no images
-    if len(images_list)==0:
-        raise ValueError(f'No images found in {file_path}')
+    # Raise error if PDF has no images
+    if len(images_list) == 0:
+        raise ValueError(f"No images found in {file_path}")
 
-    #Save all the extracted images
+    # Save all the extracted images
     for i, img in enumerate(images_list, start=1):
-        #Extract the image object number
+        # Extract the image object number
         xref = img[0]
-        #Extract image
+        # Extract image
         base_image = pdf_file.extract_image(xref)
-        #Store image bytes
-        image_bytes = base_image['image']
-        #Store image extension
-        image_ext = 'png'
-        #Generate image file name
+        # Store image bytes
+        image_bytes = base_image["image"]
+        # Store image extension
+        image_ext = "png"
+        # Generate image file name
         image_name = f"{os.path.splitext(file_path)[0]}_{i}.{image_ext}"
-        #Save image
-        with open(os.path.join(images_dir, image_name) , 'wb') as image_file:
+        # Save image
+        with open(os.path.join(images_dir, image_name), "wb") as image_file:
             image_file.write(image_bytes)
             image_file.close()
     print(f"共{i}張圖片")
